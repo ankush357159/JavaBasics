e68f2bc18958a2b3d8ff66692e1e1742d448316b
ankush357159 <ankush357159@gmail.com>
2021-08-24 21:23:36 +0530

    array examples

---
+++ src/javaBasics/TestAnonymousArray.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+public class TestAnonymousArray {
+	
+	static void printArray(int arr[]) {
+		for (int i=0; i<arr.length; i++)
+			System.out.println(arr[i]);
+	}
+
+	public static void main(String[] args) {
+		printArray(new int[] {12,13,14,15}); //passing anonymous array to method
+
+	}
+
+}
---
+++ src/javaBasics/TestReturnArray.java
@@ -0,0 +1,18 @@
+package javaBasics;
+
+public class TestReturnArray {
+	
+	static int[] get() {
+		return new int[]{1,2,3,4,5};
+	}
+
+	public static void main(String[] args) {
+		int arr[] = get();
+		
+		for (int i=0; i<arr.length; i++) 
+			System.out.println(arr[i]);
+		
+
+	}
+
+}
